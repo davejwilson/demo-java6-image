ecb4d65e9b008a214dc471fa977e832eada945d8
davejwilson <davejwilson@gmail.com>
2020-09-23 17:31:24 +0200

    fixed host url bug, added some output for exceptions, added proper resource closure block

--- src/main/java/org/demo/data/DataService.java
+++ src/main/java/org/demo/data/DataService.java
@@ -17,27 +17,50 @@ public class DataService {
             if (rootPassword == null) {
                 throw new RuntimeException("Environment MARIADB_ROOT_PASSWORD is not set");
             }
-            connection = DriverManager.getConnection("jdbc:mysql://" + hostname + "localhost:3306",
+            connection = DriverManager.getConnection("jdbc:mysql://" + hostname + ":3306",
                     "root", rootPassword);
         } catch (SQLException throwables) {
+            throwables.printStackTrace();
             throw new RuntimeException("failed to connect to mysql db", throwables);
         }
     }
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws RuntimeException {
         DataService dataService = new DataService();
         System.out.println(dataService.getHelpKeywords());
     }
 
-    public List<String> getHelpKeywords() throws SQLException {
-        PreparedStatement statement = connection.prepareStatement("select * from mysql.help_keyword");
-        ResultSet resultSet = statement.executeQuery();
-        List<String> keywords = new ArrayList<String>();
-        while (resultSet.next()) {
-            keywords.add(resultSet.getString("name"));
+    public List<String> getHelpKeywords() throws RuntimeException {
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.prepareStatement("select * from mysql.help_keyword");
+            resultSet = statement.executeQuery();
+            List<String> keywords = new ArrayList<String>();
+            while (resultSet.next()) {
+                keywords.add(resultSet.getString("name"));
+            }
+            return keywords;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("failed to get help keywords", e);
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception e) {
+                    System.err.println("Warning: failed to close resultset");
+                    // ignore, we have tried our best
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception e) {
+                    System.err.println("Warning: failed to close statement");
+                    // ignore, we have tried our best
+                }
+            }
         }
-        resultSet.close();
-        statement.close();
-        return keywords;
     }
 }
